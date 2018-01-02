094aa65ba2a50b6c9142a6ebe3386d1e6e4aac90
wujianping <1043375451@qq.com>
2018-01-02 13:39:27 +0800

    提交

--- test.js
+++ test.js
@@ -2,3 +2,46 @@
 //require('./a.js')
 
 
+let url = 'http://www.kege.com?ww=1111&dd=222';
+
+function queryUrlFormat1(url){
+  let obj = {};
+  if(url.indexOf('?') < 0) return obj;
+  url = url.split('?')[1].split('&');
+  for(let i=0;i<url.length;i++){
+    let temp = url[i].split('=');
+    obj[temp[0]] = temp[1];
+  }
+  return obj;
+}
+
+
+function queryUrlFormat2(url) {
+  let obj = {};
+  let reg = /([^?&=]+)=([^?&=]+)/g
+  url.replace(reg,function () {
+    obj[arguments[1]] = arguments[2];
+  });
+  return obj;
+}
+
+
+String.prototype.myUrlFormat1 = function () {
+  let obj = {};
+  let reg = /([^?&=]+)=([^?&=]+)/g
+  this.replace(reg,function () {
+    obj[arguments[1]] = arguments[2];
+  });
+  return obj;
+};
+
+
+
+String.prototype.myUrlFormat2 = function () {
+  let obj = {};
+  let reg = /([^?&=]+)=([^?&=]+)/g
+  this.replace(reg,(...arg) => {
+    obj[arg[1]] = arg[2];
+  });
+  return obj;
+};
